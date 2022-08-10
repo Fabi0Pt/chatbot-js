b5c55dc1b8a44d9eea50102ffedd5802606fcd46
Fábio Henro <fabi0_pt@hotmail.com>
2022-08-10 13:56:09 -0300

    remove aws

--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
 import express from "express";
 import { connectionChat } from "./config/connectionChatbot.js";
-import puppeteer from "puppeteer";
 const app = express();
 
-const browser = await puppeteer.launch({
-  args: ["--no-sandbox", "--disable-setuid-sandbox"],
-});
+
 
 connectionChat();
 
--- src/server.js
+++ src/server.js
@@ -1,3 +1,8 @@
 import app from "./app.js";
+import puppeteer from "puppeteer";
+
+const browser = await puppeteer.launch({
+  args: ["--no-sandbox", "--disable-setuid-sandbox"],
+});
 
 app.listen(3000, () => console.log(`Servidor rodando!`));
