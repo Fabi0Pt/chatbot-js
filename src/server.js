fa91068e3e93130f7d6d4fd9c12c7a8e581467cc
Fábio Henro <fabi0_pt@hotmail.com>
2022-08-10 13:59:08 -0300

    ignoreDefaultArgs

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import puppeteer from "puppeteer";
 
 const browser = await puppeteer.launch({
   args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  ignoreDefaultArgs: ["--disable-extensions"],
 });
 
 app.listen(3000, () => console.log(`Servidor rodando!`));
